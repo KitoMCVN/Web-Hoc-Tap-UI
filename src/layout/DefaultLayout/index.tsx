6f251dbf50d1f1ed6e20dfc8d4db02054be6303d
KitoMC <95675413+KitoMine@users.noreply.github.com>
2024-02-23 13:19:19 +0700

    📺 Upd: UI, header and sidebar

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -3,11 +3,13 @@ import Sidebar from "../Sidebar";
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='max-h-dvh w-full overflow-hidden bg-white dark:bg-slate-800'>
-      <Header></Header>
-      <div className="flex">
-        <Sidebar></Sidebar>
-        <div className="h-dvh">{children}</div>
+    <div className='max-h-dvh w-full overflow-hidden bg-white dark:bg-slate-900'>
+      <div className="bg-custom">
+        <Header></Header>
+        <div className="flex">
+          <Sidebar></Sidebar>
+          <div className="h-dvh">{children}</div>
+        </div>
       </div>
     </div>
   );
